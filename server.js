89743a97210b3421c5fc7d2f612444dcb794053b
emily <mle4d@me.com>
2019-06-23 22:26:51 -0700

    step 4 console longs

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require("http");
 function onRequest(request, response) {
+    console.log("Request recieved.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("hello world");
     response.end();
 }
 http.createServer(onRequest).listen(8888);
+
+console.log("server has started.");
